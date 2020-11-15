dcbcc3544ceb163fc05b039dc38e7d243d51be1f
Mete Alp Kızılçay <kizilcaymetealp@gmail.com>
2020-11-15 15:48:14 +0300

    Initial Commit

---
+++ Mapiever/MapieverApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapieverApp.swift
+//  Mapiever
+//
+//  Created by Mete Alp Kizilcay on 15.11.2020.
+//
+
+import SwiftUI
+
+@main
+struct MapieverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
